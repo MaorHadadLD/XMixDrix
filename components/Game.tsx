816b72bda77e54204d456d8c168fca7280a40055
MaorHadadLD <maorhadad94@gmail.com>
2024-04-03 03:14:10 +0300

    updating the backgroung style

--- components/Game.tsx
+++ components/Game.tsx
@@ -85,6 +85,8 @@ const Game = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        width: '100%',
+        height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
     },
